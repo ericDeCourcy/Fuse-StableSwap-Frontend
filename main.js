c35abba30d65c9ecd324bb2ba0c4ed91d13527f9
Eric DeCourcy <eric@openzeppelin.com>
2021-12-19 10:38:47 -0700

    prompts network change to fuse - TODO: doesn't update GUI correctly

--- main.js
+++ main.js
@@ -198,6 +198,35 @@ async function connectToMetamask(button) {
   } finally {
     button.disabled = false;
   }
+
+  try {
+    await ethereum.request({
+      method: 'wallet_switchEthereumChain',
+      params: 
+        [{chainId: '0x7a'  }],
+    });
+  } catch (switchError) {
+    showError(statusElement, 'Chain ID Switch');
+
+    if (switchError.code === 4902) {
+      try {
+        await ethereum.request({
+          method: 'wallet_addEthereumChain',
+          params: [{chainId: '0x7a', 
+                    chainName: 'Fuse',
+                    nativeCurrency: {
+                      name: 'Fuse',
+                      symbol: 'FUSE',
+                      decimals: 18,
+                    },
+                    rpcUrl: 'https://rpc.fuse.io'
+                  }],
+        });
+      } catch (addError) {
+        // TODO: alannnnaaaaaa
+      }
+    }
+  }
 }
 
 async function approveToken(button, tokenIndex, statusElement) {
